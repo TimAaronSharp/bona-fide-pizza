3a67f186cc522ecd56a25786732c1010e113f2e7
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-09-07 14:43:38 -0600

    Created basic route segments and page.tsx for menu and order-now routes. Deleted boilerplate html in app/page.tsx.

---
+++ app/menu/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function MenuPage() {
+  return (
+    <>
+      <main>
+        <h1>Menu Page</h1>
+      </main>
+    </>
+  )
+}
---
+++ app/order-now/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function OrderNowPage() {
+  return (
+    <>
+      <main>
+        <h1>Order Now Page</h1>
+      </main>
+    </>
+  )
+}
